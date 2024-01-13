fd833f97afc89758894bb44355c134018ccc5c32
FahadR004 <138984346+FahadR004@users.noreply.github.com>
2024-01-13 15:51:30 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-if [ "$1" == "-e" ]; then
-    ./mail
-else
-    # Fetch data 24 times
-    for ((i=0; i<24; i++))
-    do
-        # Execute datafetching script
-        ./datafetching
+gcc datafetching.c mail.c -o datafetching -ljansson -lcurl
+# Fetch data 24 times
+for ((i=0; i<24; i++))
+do
+	./datafetching
 
-        # Wait for a specified interval (e.g., 10 seconds)
-        sleep 1
-    done
+	# Wait for a specified interval (e.g., 10 seconds)
+	sleep 5
+done
 
-    # Perform calculations
-    ./calculation2
+# Perform calculations
+gcc calculation2.c -o calculation2
+./calculation2
 
 fi
 
